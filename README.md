e775a292f68ec05a4d898a41f03d78dddf581575
n0b0dy <n0b0dypku@gmail.com>
2018-08-23 05:19:21 +0000

    Add init.sh and README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Init-Ubuntu
+
+Install packages commonly used by ctfers for ubuntu, including:
+
+* Some command line tools
+* Tmux
+* Docker & docker-compose
+* Vim with my vim configuration
+
---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+set -x
+
+sudo apt update
+
+cd ~
+sudo apt -y install git zsh nginx tmux curl wget python3 vim cmake build-essential python3-distutils
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/n0b0dyCN/n0vim.git
+cd n0vim
+sh install-n0vim.sh
+cd ~
+
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker `whoami`
+cd ~
+rm get*
+rm -rf n0vim
+mkdir workspace
+mkdir tools
+mkdir ctf
+
